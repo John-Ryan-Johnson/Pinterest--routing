9a7419cc6c683b6dec56ad46d6d3e141ade753cf
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-01-04 12:39:38 -0600

    added auth

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Redirect, Switch,
 } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConnection from '../helpers/data/connection';
 import './App.scss';
 import Home from '../components/pages/Home/Home';
 import Auth from '../components/pages/Auth/Auth';
@@ -18,10 +21,26 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
+firebaseConnection();
+
 class App extends React.Component {
   state = {
-    authed: true,
+    authed: false,
   };
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+
+  }
 
   render() {
     const { authed } = this.state;
