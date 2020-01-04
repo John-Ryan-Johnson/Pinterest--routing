fb0e86548a13d645574dffa1d24c856c3dcda421
John-Ryan-Johnson <john4johnkids@gmail.com>
2020-01-04 12:23:37 -0600

    added links

--- src/App/App.js
+++ src/App/App.js
@@ -35,7 +35,6 @@ class App extends React.Component {
             <PrivateRoute path="/board/:boardId" exact component={SingleBoard} authed={authed}/>
           </Switch>
         </Router>
-
       </div>
     );
   }
